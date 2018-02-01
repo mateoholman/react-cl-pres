6fd98523cfe82368496141fe3c661452c0b749f1
Matthew Holman <mateoholman@gmail.com>
2018-02-01 16:10:35 -0600

    Proper importing of slides

--- presentation/index.js
+++ presentation/index.js
@@ -65,55 +65,6 @@ export default class Presentation extends React.Component {
           <Heading size={4} textColor="secondary">Where do we start?</Heading>
           <img src={confusedfez} />
         </Slide>
-
-        <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
-          <Heading size={4} textColor="secondary">Bootstrap!</Heading>
-          <img src={bootstrap} height="200px" width="200px" />
-        </Slide>
-
-        <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHeaderItem></TableHeaderItem>
-                <TableHeaderItem bold>react-bootstrap</TableHeaderItem>
-                <TableHeaderItem textAlign="right">reactstrap</TableHeaderItem>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableItem>Version</TableItem>
-                <TableItem>0.32.1</TableItem>
-                <TableItem>4.8.0</TableItem>
-              </TableRow>
-              <TableRow>
-                <TableItem>GitHub Stars</TableItem>
-                <TableItem>12,110</TableItem>
-                <TableItem>3,346</TableItem>
-              </TableRow>
-              <TableRow>
-                <TableItem># Components</TableItem>
-                <TableItem>26</TableItem>
-                <TableItem>26</TableItem>
-              </TableRow>
-              <TableRow>
-                <TableItem># Contributors</TableItem>
-                <TableItem>198</TableItem>
-                <TableItem>79</TableItem>
-              </TableRow>
-              <TableRow>
-                <TableItem>Open Bugs / Issues</TableItem>
-                <TableItem>4 / 43</TableItem>
-                <TableItem>5 / 70</TableItem>
-              </TableRow>
-              <TableRow>
-                <TableItem>License</TableItem>
-                <TableItem>MIT</TableItem>
-                <TableItem>MIT</TableItem>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </Slide>
         {bootstrapSlides}
       </Deck>
     );
